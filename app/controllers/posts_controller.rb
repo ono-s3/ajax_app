16f231301a194f6ee77382631e91d85b7730796a
ono-s3 <ono.s.3mail@gmail.com>
2021-01-07 19:16:40 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      # 既読であれば「既読を解除するためにfalseへ変更」
+      post.update(checked: false)
+    else
+      # 既読でなければ「既読にするためtrueへ変更」
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
